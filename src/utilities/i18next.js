a76b1a62473b16c239043026c7ada7bb88aa29d3
whphhg <whphhg@protonmail.ch>
2017-07-20 13:21:38 +0200

    Add i18next utility

---
+++ src/utilities/i18next.js
@@ -0,0 +1,38 @@
+import i18next from 'i18next'
+import fetch from 'isomorphic-unfetch'
+
+/**
+ * Initialize a i18next instance.
+ * @function i18n
+ * @param {object} resources - Translations.
+ * @param {string} lng - Language.
+ * @return {object} i18next instance.
+ */
+export const i18n = (resources, lng) => {
+  return i18next.init({
+    debug: false,
+    defaultNS: 'common',
+    fallbackLng: 'en-US',
+    lng,
+    ns: ['common'],
+    resources
+  })
+}
+
+/**
+ * Fetch translation file(s).
+ * @function fetchTranslation
+ * @param {string} language - Language to fetch.
+ * @param {array} files - Translation files to fetch.
+ * @param {string} url - Url where locales are available.
+ */
+export async function fetchTranslation (language, files, url) {
+  let translation = {}
+
+  for (let file of files) {
+    const response = await fetch(''.concat(url, language, '/', file, '.json'))
+    translation[file] = await response.json()
+  }
+
+  return { [language]: translation }
+}
