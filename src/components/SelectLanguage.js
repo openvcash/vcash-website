d7f7ec338b27c4bf46847e1a9cb22e0f3f602450
whphhg <whphhg@protonmail.ch>
2017-07-21 16:51:36 +0200

    Add SelectLanguage

---
+++ src/components/SelectLanguage.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import { translate } from 'react-i18next'
+import { Select } from 'antd'
+import { set as setCookie } from 'js-cookie'
+
+@translate(['common'])
+class SelectLanguage extends React.Component {
+  constructor (props) {
+    super(props)
+    this.t = props.t
+    this.language = props.i18n.language
+
+    /** Available languages. */
+    this.languages = [
+      { language: 'en-US', name: 'English' },
+      { language: 'sl-SI', name: 'Slovenian' }
+    ]
+  }
+
+  /**
+   * Set display language and save it to cookies.
+   * @function setLanguage
+   * @param {string} language - Display language.
+   */
+  setLanguage (language) {
+    setCookie('language', language, { expires: 365 })
+    document.location.reload()
+  }
+
+  render () {
+    return (
+      <div className='flex'>
+        <div className='flex'>
+          <i className='material-icons md-16'>language</i>
+          <p>
+            {this.t('selectLanguage')}
+          </p>
+        </div>
+        <Select
+          defaultValue={this.language}
+          notFoundContent={this.t('languageNotFound')}
+          onChange={language => this.setLanguage(language)}
+          optionFilterProp='children'
+          showSearch
+          size='small'
+          style={{ margin: '0 0 0 32px', width: '120px' }}
+        >
+          {this.languages.map(entry =>
+            <Select.Option key={entry.language} value={entry.language}>
+              {entry.name}
+            </Select.Option>
+          )}
+        </Select>
+      </div>
+    )
+  }
+}
+
+export default SelectLanguage
