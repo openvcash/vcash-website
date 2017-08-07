abd659aa6eadc68291e604a2563798e47a38625e
whphhg <whphhg@protonmail.ch>
2017-08-07 12:24:15 +0200

    Add shortUid() to common utilities

--- src/utilities/common.js
+++ src/utilities/common.js
@@ -7,3 +7,15 @@
 export const shorten = (text, n) => {
   return text.length > n ? ''.concat(text.substr(0, n - 1), '..') : text
 }
+
+/**
+ * Get a 4-character alphanumeric unique sequence.
+ * For N unique IDs, out of X possibilities,
+ * call at most 1 / (1 − N / X) times on average to ensure uniqueness.
+ * @function shortUid
+ * @return {string} Unique 4-character uid.
+ * @see {@link http://stackoverflow.com/a/6248722|StackOverflow}
+ */
+export const shortUid = () => {
+  return ('0000' + ((Math.random() * 1679616) | 0).toString(36)).slice(-4)
+}
