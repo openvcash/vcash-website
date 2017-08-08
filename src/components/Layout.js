70e8171bc758f3d1159d3139564637f1c31c96c9
whphhg <whphhg@protonmail.ch>
2017-08-08 19:29:27 +0200

    Add Layout component

---
+++ src/components/Layout.js
@@ -0,0 +1,11 @@
+import Footer from './Footer'
+import Header from './Header'
+
+const Layout = props =>
+  <div className='body'>
+    <Header />
+    {props.children}
+    <Footer />
+  </div>
+
+export default Layout
