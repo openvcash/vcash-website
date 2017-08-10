dde8e70596f59745d4792dd95817d0d040b472ee
whphhg <whphhg@protonmail.ch>
2017-08-10 21:00:51 +0200

    RechartsCustom: add CustomTick and improve CustomLabel

--- src/components/charts/RechartsCustom.js
+++ src/components/charts/RechartsCustom.js
@@ -1,6 +1,41 @@
 import React from 'react'
 
 /**
+ * Custom axis tick.
+ * @function CustomTick
+ */
+export const CustomTick = props => {
+  let value = ''
+
+  switch (props.textType) {
+    case 'amount':
+      value = props.payload.value + ' XVC'
+      break
+
+    case 'number':
+      value = new Intl.NumberFormat(props.language).format(props.payload.value)
+      break
+
+    default:
+      value = props.payload.value
+      break
+  }
+
+  return (
+    <g transform={`translate(${props.x},${props.y})`}>
+      <text
+        fill='#666666'
+        textAnchor={props.textAnchor || 'end'}
+        x={props.textX || 0}
+        y={props.textY || 0}
+      >
+        {value}
+      </text>
+    </g>
+  )
+}
+
+/**
  * Custom tooltip.
  * @function CustomTooltip
  */
@@ -9,13 +44,25 @@ export const CustomTooltip = props => {
 
   return (
     <div className='chartTooltip'>
+      {props.hasOwnProperty('labelText') === true &&
+        <p className='label'>
+          {props.labelText}{' '}
+          {props.labelType === 'number' &&
+            new Intl.NumberFormat(props.language).format(props.label)}
+        </p>}
+
       {props.payload.map(entry =>
         <div className='flex-sb' key={entry.name}>
-          <p style={{ margin: '0 72px 0 0' }}>
+          <p style={{ color: entry.color, margin: '0 72px 0 0' }}>
             {entry.name}
           </p>
-          <p style={{ fontWeight: 'bold' }}>
-            {entry.value}
+          <p style={{ color: entry.color, fontWeight: '500' }}>
+            {(props.amounts === true &&
+              new Intl.NumberFormat(props.language, {
+                minimumFractionDigits: 6,
+                maximumFractionDigits: 6
+              }).format(entry.value) + ' XVC') ||
+              (props.amounts !== true && entry.value)}
           </p>
         </div>
       )}
