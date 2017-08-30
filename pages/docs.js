4e095c445118b01bb0d964908ccacabc402cb703
whphhg <whphhg@protonmail.ch>
2017-08-30 22:24:25 +0200

    Add docs page

---
+++ pages/docs.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import { I18nextProvider } from 'react-i18next'
+import { Provider } from 'mobx-react'
+import { parse as parseCookie } from 'cookie'
+import { get as getCookie } from 'js-cookie'
+import { i18n, fetchTranslation } from '../src/utilities/i18next'
+
+/** Required comonents. */
+import Layout from '../src/components/Layout'
+import Docs from '../src/components/Docs'
+
+/** Required stores. */
+import { initDocs } from '../src/stores/docs'
+
+class DocsPage extends React.Component {
+  static async getInitialProps ({ req }) {
+    const initProps = {
+      isServer: typeof window === 'undefined',
+      language: 'en-US',
+      translation: {}
+    }
+
+    /**
+     * Read the language cookie from the request headers if on the server,
+     * or read the cookie directly if on the client.
+     */
+    if (initProps.isServer === true) {
+      const cookie = parseCookie(req.headers.cookie)
+      initProps.language = cookie.language
+    } else {
+      initProps.language = getCookie('language')
+    }
+
+    /** Fetch the translation files for the language found in the cookie. */
+    initProps.translation = await fetchTranslation(
+      initProps.language,
+      ['common'],
+      'http://localhost:3000/static/locales/'
+    )
+
+    /** Return initial properties. */
+    return initProps
+  }
+
+  constructor (props) {
+    super(props)
+    this.i18n = i18n(props.translation, props.language)
+    this.docs = initDocs(props.isServer)
+  }
+
+  render () {
+    return (
+      <Provider docs={this.docs}>
+        <I18nextProvider i18n={this.i18n}>
+          <Layout>
+            <Docs id={this.props.url.query.id} />
+          </Layout>
+        </I18nextProvider>
+      </Provider>
+    )
+  }
+}
+
+export default DocsPage
